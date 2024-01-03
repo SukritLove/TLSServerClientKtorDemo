80ed8304a12476bd6b4214ad91af537e1d4fe470
SukritLove <sukritlove2712@gmail.com>
2024-01-03 14:39:30 +0700

    Add UI

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -8,8 +8,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -20,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import data.ClientMessage
 import kotlinx.coroutines.Dispatchers
@@ -34,6 +38,10 @@ import util.startServer
 @Composable
 fun App(networkUtils: PlatformNetworkingUtils) {
     var deviceIpAddress by remember { mutableStateOf<String?>("") }
+    var inputIpAddress by remember { mutableStateOf("") }
+    var inputPort by remember { mutableStateOf("4001") }
+    var inputMessage by remember { mutableStateOf("") }
+    var clientStarted by remember { mutableStateOf(false) }
     val message = ClientMessage.messages.message
 
 
@@ -69,6 +77,66 @@ fun App(networkUtils: PlatformNetworkingUtils) {
                 modifier = Modifier.padding(10.dp)
             )
         }
+
+
+        AddSpace()
+        CreateTextField(
+            value = inputIpAddress,
+            onTextChange = { inputIpAddress = it },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            placeholder = "Client IP"
+        )
+
+        Spacer(modifier = Modifier.padding(10.dp))
+        CreateTextField(
+            value = inputPort,
+            onTextChange = { inputPort = it },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            placeholder = "Client Port"
+        )
+
+        AddSpace()
+        CreateTextField(
+            value = inputMessage,
+            onTextChange = { inputMessage = it },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            placeholder = "Message"
+        )
+
+        Spacer(modifier = Modifier.padding(10.dp))
+        CreateButton(
+            btnText = "Send Message",
+            onClickAction = {
+                clientStarted = !clientStarted
+            })
+
+
     }
 
+}
+
+@Composable
+fun CreateTextField(
+    value: String,
+    onTextChange: (String) -> Unit,
+    keyboardOptions: KeyboardOptions,
+    placeholder: String
+) {
+    TextField(value = value,
+        onValueChange = onTextChange,
+        keyboardOptions = keyboardOptions,
+        placeholder = { Text(text = placeholder) })
+}
+
+@Composable
+fun CreateButton(btnText: String, onClickAction: () -> Unit) {
+    Button(onClick = onClickAction) {
+        Text(text = btnText)
+    }
+}
+
+
+@Composable
+fun AddSpace() {
+    Spacer(modifier = Modifier.padding(20.dp))
 }
